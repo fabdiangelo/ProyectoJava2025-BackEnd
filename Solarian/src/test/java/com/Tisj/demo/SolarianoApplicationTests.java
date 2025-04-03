fd07b425d76216389f3b1750e1a2369729577f41
ZimmZimmGames <alexzimmer.zimmer1@gmail.com>
2025-04-03 15:24:08 -0300

    Init proyect

---
+++ Solarian/src/test/java/com/Tisj/demo/SolarianoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tisj.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolarianoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
