4cf663e597047ba09be3e94318c93a51f949ec5a
Fabricio Fuentes <fuentesfabricio17@gmail.com>
2025-04-05 19:34:44 -0300

    swagger

--- src/main/java/com/Tisj/SolarianoApplication.java
+++ src/main/java/com/Tisj/SolarianoApplication.java
@@ -1,4 +1,4 @@
-package com.Tisj.demo;
+package com.Tisj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/Tisj/utils/OpenApiConfig.java
@@ -0,0 +1,26 @@
+package com.Tisj.utils;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public GroupedOpenApi publicApi(){
+        return GroupedOpenApi.builder()
+                .group("tallerJava")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI openAPI(){
+        return new OpenAPI().info(
+                new Info().version("v1").description("api test")
+        );
+    }
+}
+
