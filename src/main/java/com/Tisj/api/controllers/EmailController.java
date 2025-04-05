0d059c3bb3977954afd093249ebf35863cee873e
Fabricio Fuentes <fuentesfabricio17@gmail.com>
2025-04-05 20:27:16 -0300

    API mail (html)

--- src/main/java/com/Tisj/api/controllers/EmailController.java
+++ src/main/java/com/Tisj/api/controllers/EmailController.java
@@ -1,6 +1,7 @@
 package com.Tisj.api.controllers;
 
 import com.Tisj.services.EmailService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/email")
 public class EmailController {
@@ -15,10 +17,24 @@ public class EmailController {
     @Autowired
     private EmailService emailService;
 
-    @GetMapping("/enviar")
+    @GetMapping("prueba/string")
     public ResponseEntity<String> enviarMail(@RequestParam("destinatario") String destinatario,
                                              @RequestParam("asunto") String asunto) {
-        emailService.enviarCorreo(destinatario, asunto, "Probando");
+        try {
+            emailService.enviarCorreo(destinatario, asunto, "Probando", false);
+        }catch (Exception e){
+            log.error("Error: ", e);
+        }
+        return ResponseEntity.ok("Correo enviado");
+    }
+
+    @GetMapping("prueba/html")
+    public ResponseEntity<String> enviarMail(@RequestParam("destinatario") String destinatario) {
+        try {
+            emailService.notiCompra(destinatario);
+        } catch (Exception e) {
+            log.error("Error: ", e);
+        }
         return ResponseEntity.ok("Correo enviado");
     }
 }
--- src/main/java/com/Tisj/services/EmailService.java
+++ src/main/java/com/Tisj/services/EmailService.java
@@ -1,9 +1,17 @@
 package com.Tisj.services;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @Service
 public class EmailService {
@@ -11,14 +19,23 @@ public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
-    public void enviarCorreo(String to, String subject, String body) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(System.getenv("CORREO"));
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(body);
+    public void enviarCorreo(String to, String subject, String body, boolean isHtml) throws MessagingException {
+        MimeMessage mensaje = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mensaje, true);
 
-        mailSender.send(message);
+        helper.setFrom(System.getenv("CORREO"));
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(body, isHtml);
+
+        mailSender.send(mensaje);
+    }
+
+    public void notiCompra(String to) throws IOException, MessagingException {
+        ClassPathResource resource = new ClassPathResource("templates/notificarCompra.html");
+        String htmlContent = Files.readString(Path.of(resource.getURI()));
+
+        enviarCorreo(to, "Nueva Compra", htmlContent, true);
     }
 }
 
