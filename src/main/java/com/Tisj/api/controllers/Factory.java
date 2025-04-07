2e6b56fdf4d7def770ea44ae7d6a9add72252052
ZimmZimmGames <alexzimmer.zimmer1@gmail.com>
2025-04-07 17:16:28 -0300

    InitSystem

--- src/main/java/com/Tisj/api/controllers/Factory.java
+++ src/main/java/com/Tisj/api/controllers/Factory.java
@@ -1,4 +1,34 @@
 package com.Tisj.api.controllers;
 
+import com.Tisj.api.Interfaces.ISystem;
+import com.Tisj.bussines.entities.*;
+
+
+
 public class Factory {
+
+    public ISystem crearSystem() {
+        return System.getInstance();
+    }
+
+    public Cliente crearCliente() {
+        return new Cliente();
+    }
+
+    public ContentCreator crearContentCreator() {
+        return new ContentCreator();
+    }
+
+    public Admin crearAdmin() {
+        return new Admin();
+    }
+
+    public Carrito crearCarrito() {
+        return new Carrito();
+    }
+
+    public Pago CrearPago() {
+        return new Pago();
+    }
 }
+
--- src/main/java/com/Tisj/api/controllers/System.java
+++ src/main/java/com/Tisj/api/controllers/System.java
@@ -1,4 +1,80 @@
 package com.Tisj.api.controllers;
 
-public class System {
+
+import com.Tisj.api.Interfaces.ISystem;
+import com.Tisj.bussines.entities.*;
+import java.util.List;
+
+public class System implements ISystem {
+
+    private static System instance; // Instancia única (Singleton)
+
+    private System() {
+        // Constructor privado para evitar la creación de instancias desde fuera
+    }
+
+    public static System getInstance() {
+        if (instance == null) {
+            instance = new System();
+        }
+        return instance;
+    }
+
+
+    @Override
+    public Usuario RegistrarUsuario() {
+        return null;
+    }
+
+    @Override
+    public void ModificarUsuario() {
+
+    }
+
+    @Override
+    public void EliminarUsuario() {
+
+    }
+
+    @Override
+    public void ModificarCurso() {
+
+    }
+
+    @Override
+    public void EliminarCurso() {
+
+    }
+
+    @Override
+    public void ModificarPaquete() {
+
+    }
+
+    @Override
+    public void EliminarPaquete() {
+
+    }
+
+    @Override
+    public List<Usuario> ListarUsuario() {
+        return List.of();
+    }
+
+    @Override
+    public Pago CrearPago() {
+        return null;
+    }
+
+    @Override
+    public System crearSystem() {
+        return null;
+    }
+
+    @Override
+    public Carrito crearCarrito() {
+        return null;
+    }
+
+
 }
