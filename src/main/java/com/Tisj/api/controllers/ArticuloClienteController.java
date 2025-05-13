82e051cdf5f7561b30a65d0dd4651fdebe6cc384
ZimmZimmGames <alexzimmer.zimmer1@gmail.com>
2025-05-13 16:55:31 -0300

    Update ArticuloClienteController.java

--- src/main/java/com/Tisj/api/controllers/ArticuloClienteController.java
+++ src/main/java/com/Tisj/api/controllers/ArticuloClienteController.java
@@ -2,7 +2,6 @@ package com.Tisj.api.controllers;
 
 import com.Tisj.bussines.entities.ArticuloCliente;
 import com.Tisj.services.ArticuloClienteService;
-import com.Tisj.api.pojo.ArticuloClienteDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
